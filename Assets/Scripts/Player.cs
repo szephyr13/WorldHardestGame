fc4fa932db4e9e056d045c7b6a0c048b0d3a6da6
Azazel <80421328+szephyr13@users.noreply.github.com>
2025-01-18 13:37:12 +0100

    UI not functional

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,11 +10,23 @@ public class Player : MonoBehaviour
 
     public int score;
     public int lifes;
+
+    private bool check2;
+    private bool check3;
+    private bool check4;
+
+    private Vector3 origin;
+    private Vector3 newStartPoint;
     // Start is called before the first frame update
     void Start()
     {
         score = 0;
         lifes = 3;
+        check2 = false;
+        check3 = false;
+        check4 = false;
+        origin = new Vector3(-8.0f, 3.38f, 0);
+        newStartPoint = origin;
     }
 
     // Update is called once per frame
@@ -43,13 +55,42 @@ public class Player : MonoBehaviour
     {
         if (collision.CompareTag("Boundary"))
         {
-            transform.position = new Vector3(-8, 3.38f, 0);
+            transform.position = newStartPoint;
         }
 
         if (collision.CompareTag("Collect"))
         {
             score++;
             Destroy(collision.gameObject);
+        }
+
+        if (collision.CompareTag("Check2"))
+        {
+            if (check2 == false)
+            {
+                newStartPoint = new Vector3(-0.41f, -3.68f, 0);
+                score += 10;
+                check2 = true;
+            }
+        }
+
+        if (collision.CompareTag("Check3"))
+        {
+            if (check3 == false)
+            {
+                newStartPoint = new Vector3(4.3f, -0.1f, 0);
+                score += 20;
+                check3 = true;
+            }
+        }
+
+        if (collision.CompareTag("Win"))
+        {
+            if (check4 == false)
+            {
+                score += 5;
+                check4 = true;
+            }
         }
     }
 }
